3481480f69f2a4b7ec19851d4780559b3404ae66
silkyclouds <silkyclouds@users.noreply.github.com>
2023-09-05 21:18:50 +0200

    Update noise_buster.py
    Added a new way to monitor if influxDB is still alive
    implemented the csv logging back, aside the influxDB method. Could be useful for people willing to analyse their logs using excel or other similar tools
    Added DB adjustment for distance

--- noise_buster.py
+++ noise_buster.py
@@ -1,104 +1,102 @@
 import logging
 import sys
 import usb.core
-import sched
 import time
-import logging
 import traceback
-
-from threading import Timer
 from datetime import datetime
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 from pushover import Client
 
-# Noise levels configuration
+# Configure logging level and output format
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Create a logger
 logger = logging.getLogger(__name__)
 
-##############################################################################
-#CONFIGUE THE BELOW SETTINGS TO YOUR CONVENIENCE
-##############################################################################
+# --------------------- Configuration Section ---------------------
 
-# InfluxDB connection information
-influxdb_host = "127.0.0.1"  # Set the InfluxDB host address (e.g., "192.168.194.240")
-influxdb_port = 8086  # Set the InfluxDB port (default: 8086)
-influxdb_token = ""  # Set the InfluxDB token (within double quotes)
-influxdb_org = "noise_buster"  # Set the InfluxDB organization name (e.g., "noise_buster")
-influxdb_bucket = "noise_buster"  # Set the InfluxDB bucket name (e.g., "noise_buster")
-influxdb_timeout = 20000  # Set the InfluxDB timeout value in milliseconds (e.g., 20000)
+# InfluxDB host address (IP or URL)
+influxdb_host = "192.168.194.240"
+# InfluxDB port (usually 8086)
+influxdb_port = 8086
+# InfluxDB authentication token
+influxdb_token = "Your InfluxDB Token Here"
+# InfluxDB organization name
+influxdb_org = "noise_buster"
+# InfluxDB bucket name
+influxdb_bucket = "noise_buster"
+# InfluxDB timeout in milliseconds
+influxdb_timeout = 20000
 
-# Pushover connection information (Optional) (uncomment if you want to use pushover!)
-#pushover_user_key = "your_pushover_key_here"  # Set the Pushover user key (within double quotes) or leave empty to skip Pushover notifications
-#pushover_api_token = "your_pushover_api_token_here"  # Set the Pushover API token (within double quotes) or leave empty to skip Pushover notifications
+# Pushover user key for notifications (leave empty to disable Pushover notifications)
+pushover_user_key = "Your Pushover User Key Here"
+# Pushover API token (leave empty to disable Pushover notifications)
+pushover_api_token = "Your Pushover API Token Here"
 
-# Minimum noise level for logging events
-minimum_noise_level = 80  # Set the minimum noise level for logging events (e.g., 80)
+# Minimum noise level for logging events (in dB)
+minimum_noise_level = 80
 
-# Content of messages sent by Pushover (uncomment if you want to use pushover)
-#pushover_message = "Lets bust these noise events"  # Set the content of the Pushover message (within double quotes)
-#pushover_title = "Noise Buster"  # Set the title of the Pushover message (within double quotes)
+# Message content for Pushover notifications
+pushover_message = "Lets bust these noise events"
+# Title for Pushover notifications
+pushover_title = "Noise Buster"
 
 # Message to display when starting the script
-start_message = "Lets bust these noise events"  # Set the start message to display (within double quotes)
+start_message = "Huray, Noise Buster started"
 
-# InfluxDB measurement and location
-influxdb_measurement = "noise_buster_events"  # Set the InfluxDB measurement name (within double quotes)
-influxdb_location = "noise_buster"  # Set the location for InfluxDB measurement (within double quotes)
+# InfluxDB measurement name (where data will be written in the DB)
+influxdb_measurement = "noise_buster_events"
+# Location tag for InfluxDB measurement
+influxdb_location = "noise_buster"
 
-##############################################################################
-# DO NOT TOUCH ANYTHING BELOW THIS LINE (EXCEPT IF YOU KNOW WHAT YOU ARE DOING)
-##############################################################################
+# dB adjustment for distance (in dB) (if you happen to put your volume meter far away from the noise source, you can add dB's here to reflect a closer reality)
+dB_adjustment = 1
 
+# --------------------- End of Configuration Section ---------------------
 
-# Create a log manager for standard output
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.INFO)
-stdout_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-stdout_handler.setFormatter(stdout_formatter)
+# Counter for failed InfluxDB pings
+failed_influxdb_pings = 0
 
-# Create a logger for standard output
-stdout_logger = logging.getLogger('stdout_logger')
-stdout_logger.setLevel(logging.INFO)
-stdout_logger.addHandler(stdout_handler)
+# Last recorded dB and timestamp
+last_dB = None
+last_timestamp = None
 
-try:
-    if pushover_user_key and pushover_api_token:
-        client = Client(pushover_user_key, api_token=pushover_api_token)
-        client.send_message(pushover_message, title=pushover_title)
+# Function to check the health of the InfluxDB server
+def check_influxdb_health():
+    global failed_influxdb_pings
+    try:
+        if influxdb_client.health():
+            failed_influxdb_pings = 0
+        else:
+            failed_influxdb_pings += 1
+    except Exception as e:
+        failed_influxdb_pings += 1
 
-    dev = usb.core.find(idVendor=0x16c0, idProduct=0x5dc)
-
-    dB = 0
-
-    stdout_logger.info(start_message)
-
-    # Connect to InfluxDB
-    influxdb_client = InfluxDBClient(url=f"http://{influxdb_host}:{influxdb_port}", token=influxdb_token,
-                                     org=influxdb_org, timeout=influxdb_timeout)
-    write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
-
-    if influxdb_client.health():
-        stdout_logger.info("Connected to InfluxDB successfully.")
+    if failed_influxdb_pings >= 10:
+        message = "InfluxDB server is down. Failed to respond to 10 consecutive pings."
         if pushover_user_key and pushover_api_token:
-            client.send_message("Successfully connected to InfluxDB", title=pushover_title)
-    else:
-        stdout_logger.info("Error connecting to InfluxDB.")
-        if pushover_user_key and pushover_api_token:
-            client.send_message("Error connecting to InfluxDB", title=pushover_title)
+            client.send_message(message, title=pushover_title)
+        logger.info(message)
+        failed_influxdb_pings = 0
 
-    # Update noise level data
-    def update():
+    time.sleep(5)
+
+# Function to update noise level and log it
+def update():
+    global last_dB, last_timestamp
+    while True:
         ret = dev.ctrl_transfer(0xC0, 4, 0, 0, 200)
         global dB
         dB = (ret[0] + ((ret[1] & 3) * 256)) * 0.1 + 30
-        if dB >= minimum_noise_level:
-            timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-            stdout_logger.info('%s, %.1f dB', timestamp, round(dB, 1))
+        dB += dB_adjustment  # Apply dB adjustment based on distance
+        timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        
+        if dB >= minimum_noise_level and (dB != last_dB or timestamp != last_timestamp):
+            last_dB = dB
+            last_timestamp = timestamp
+            logger.info('%s, %.1f dB', timestamp, round(dB, 1))
 
-            # Write data to InfluxDB
             data = [
                 {
                     "measurement": influxdb_measurement,
@@ -113,13 +111,44 @@ try:
             ]
             write_api.write(influxdb_bucket, record=data)
 
-        t = Timer(0.5, update)
-        t.start()
+            # Log data to CSV
+            with open('noise.csv', 'a') as f:
+                f.write(f"{timestamp},{round(dB, 1)}\n")
 
-    # Start the update loop
+        time.sleep(0.5)
+
+try:
+    if pushover_user_key and pushover_api_token:
+        client = Client(pushover_user_key, api_token=pushover_api_token)
+        client.send_message(pushover_message, title=pushover_title)
+
+    dev = usb.core.find(idVendor=0x16c0, idProduct=0x5dc)
+
+    dB = 0
+
+    logger.info(start_message)
+
+    influxdb_client = InfluxDBClient(url=f"http://{influxdb_host}:{influxdb_port}", token=influxdb_token,
+                                     org=influxdb_org, timeout=influxdb_timeout)
+    write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
+
+    if influxdb_client.health():
+        logger.info("Connected to InfluxDB successfully.")
+        if pushover_user_key and pushover_api_token:
+            client.send_message("Successfully connected to InfluxDB", title=pushover_title)
+    else:
+        logger.info("Error connecting to InfluxDB.")
+        if pushover_user_key and pushover_api_token:
+            client.send_message("Error connecting to InfluxDB", title=pushover_title)
+
+    # Start the InfluxDB server health check in a separate thread
+    from threading import Thread
+    Thread(target=check_influxdb_health).start()
+
+    # Start updating noise level
     update()
 except Exception as e:
-   with open('error.log', 'a') as f:
+    with open('error.log', 'a') as f:
         f.write(str(e) + "\n")
         f.write(traceback.format_exc())
         logger.error(str(e))
